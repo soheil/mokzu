cbb75ca107262fa85f4ce8519df7d2d62cd97b86
Soheil <ysoheil@gmail.com>
2024-11-24 09:54:53 -0800

    Sun Nov 24 09:54:53 PST 2024

--- src/UpgradeButton.tsx
+++ src/UpgradeButton.tsx
@@ -24,16 +24,7 @@ const UpgradeButton = () => {
   return (
     <a className="upgrade-button generate-button glass-button mb-2" href={stripeLink} target="_blank">
       <span>Upgrade to Premium
-        <svg
-          width="32"
-          height="18"
-          viewBox="0 0 64 52"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="gold"
-        >
-          <path d="M2 40L10 18 20 32 32 12 44 32 54 18 62 40H2z" />
-          <rect x="2" y="40" width="60" height="10" rx="2" />
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-crown w-4 h-4 text-[#FFAE00] ml-auto" data-sentry-element="Crown" data-sentry-component="CrownPage" data-sentry-source-file="Crown.tsx"><path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z"></path><path d="M5 21h14"></path></svg>
       </span>
     </a>
   );
