2772b36f24d1bc699ca5e6172a4d5d045168f880
Soheil <ysoheil@gmail.com>
2024-12-02 16:24:31 -0800

    Mon Dec  2 16:24:31 PST 2024

--- src/UpgradeButton.tsx
+++ src/UpgradeButton.tsx
@@ -22,9 +22,9 @@ import React from 'react';
 const stripeLink = 'https://buy.stripe.com/9AQ4h478lgMs6l2bIK';
 const UpgradeButton = () => {
   return (
-    <a className="upgrade-button generate-button glass-button mb-2" href={stripeLink} target="_blank">
+    <a className="upgrade-button generate-button glass-button" href={stripeLink} target="_blank">
       <span>Upgrade to Premium
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="gold" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-crown w-4 h-4 text-[#FFAE00] ml-auto" data-sentry-element="Crown" data-sentry-component="CrownPage" data-sentry-source-file="Crown.tsx"><path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z"></path><path d="M5 21h14"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="gold" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-crown w-4 h-4 text-[#FFAE00] ml-auto" data-sentry-element="Crown" data-sentry-component="CrownPage" data-sentry-source-file="Crown.tsx"><path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z"></path><path d="M5 21h14"></path></svg>
       </span>
     </a>
   );
